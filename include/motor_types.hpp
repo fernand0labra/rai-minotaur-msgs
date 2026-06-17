b37ddc65c53c942d22ee453b54842b590017666a
Lina LTU-Ubuntu <lina.van.brugge@tum.de>
2026-06-17 13:09:58 +0200

    push all custom msg to new repo

---
+++ include/motor_types.hpp
@@ -0,0 +1,18 @@
+#ifndef MOTOR_TYPES_HPP_
+#define MOTOR_TYPES_HPP_
+
+enum motorType {
+    BAM_PAD,
+    FAM_PAD,
+    PUSHER_BPM,
+    PUSHER_DPM,
+    AUGER_PIPE
+};
+
+enum motorPos {
+    LEFT = 10,
+    RIGHT = 11,
+    TOP = 12
+};
+
+#endif
